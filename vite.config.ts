23719595a35ddaf8aad82a65dc81b2092fcf3ed9
6rahul9 <rahulmohature0421@gmail.com>
2024-10-01 02:47:00 +0530

    commit message

---
+++ vite.config.ts
@@ -0,0 +1,40 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import path from 'path';
+import { defineConfig, Plugin } from 'vite';
+import glsl from 'vite-plugin-glsl';
+import handlebars from 'vite-plugin-handlebars';
+
+export default defineConfig(({ mode }) => {
+	console.log('⚓ ' + mode)
+	return {
+		root: './src',
+		base: mode === 'development' ? '/' : '/rhombus-flow/',
+		publicDir: '../public',
+		plugins: [
+			glsl(),
+			handlebars({
+				partialDirectory: path.resolve(__dirname, './src/partials/')
+			}) as Plugin
+		],
+		build: {
+			outDir: '../dist',
+			rollupOptions: {
+				input: {
+					home: path.resolve(__dirname, './src/index.html')
+				}
+			},
+			cssCodeSplit: false,
+			emptyOutDir: true
+		},
+		server: {
+			host: true
+		}
+	}
+})
